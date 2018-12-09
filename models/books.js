b41ab69a9f1bc79bf14485f5ffebe28a4c942196
Zonghao Li <675375410@qq.com>
2018-12-09 17:53:09 +0000

    Travis generates coverage reports

--- models/books.js
+++ models/books.js
@@ -1,9 +1,4 @@
-// const books = [{id:10001,name:'War_and_Peace',recommended:500},
-//     {id:10002,name:'Adventure_of_Sherlock_Holmes',recommended:400},
-//     {id:10003,name:'We',recommended:400},
-//     {id:10004,name:'We_are_electric',recommended:400},
-//     {id:10005,name:'123_gogogo',recommended:400},
-//     {id:10006,name:"\"This is my life\"",recommended:400}];
+
 let mongoose = require('mongoose');
 let BookSchema = new mongoose.Schema({
 
